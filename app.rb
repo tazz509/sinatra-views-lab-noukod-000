f8fb0c2ddf61d3a5f17e36a327743f73a7716da4
carly <carly@noukod.com>
2019-01-11 18:50:32 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,21 @@
 class App < Sinatra::Base
 
+	get '/hello' do
+		erb :hello
+	end
+
+	get '/goodbye' do
+		erb :goodbye
+	end
+
+	get '/date' do
+		erb :date
+	end
+
 	get '/' do
 		erb :index
 	end
+
 
 
 end
